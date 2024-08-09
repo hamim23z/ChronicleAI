6980f8d768dd0632c0fa1a2e3797286f4c2bb2e8
Hamim <hamimc232@gmail.com>
2024-08-09 19:36:33 -0400

    Changed color scheme, BG image, color, etc. Mainly stylistic changes

--- app/page.js
+++ app/page.js
@@ -1,38 +1,39 @@
-'use client';
-import { Box, Stack, TextField, Button, Typography } from '@mui/material';
-import { useState } from 'react';
+"use client";
+import { Box, Stack, TextField, Button, Typography } from "@mui/material";
+import { useState } from "react";
 
 export default function Home() {
   const [messages, setMessages] = useState([
     {
-      role: 'assistant',
-      content: "Hi, I'm the FBG Support Agent, how can I assist you today?",
-    }
+      role: "assistant",
+      content:
+        "Hello! My name is Chad, the AI Support Agent for Computer Science related questions. Feel free to drop your questions down below!",
+    },
   ]);
 
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
 
   const sendMessage = async () => {
-    if (message.trim() === '') return; // Do not send empty messages
+    if (message.trim() === "") return; // Do not send empty messages
 
-    setMessage('');
+    setMessage("");
     setMessages((messages) => [
       ...messages,
-      { role: 'user', content: message },
-      { role: 'assistant', content: '' },
+      { role: "user", content: message },
+      { role: "assistant", content: "" },
     ]);
 
-    const response = fetch('/api/chat', {
+    const response = fetch("/api/chat", {
       method: "POST",
       headers: {
-        'Content-Type': 'application/json'
+        "Content-Type": "application/json",
       },
-      body: JSON.stringify([...messages, { role: 'user', content: message }]),
+      body: JSON.stringify([...messages, { role: "user", content: message }]),
     }).then(async (res) => {
       const reader = res.body.getReader();
       const decoder = new TextDecoder();
 
-      let result = '';
+      let result = "";
       return reader.read().then(function processText({ done, value }) {
         if (done) {
           return result;
@@ -52,10 +53,10 @@ export default function Home() {
         return reader.read().then(processText);
       });
     });
-  }
+  };
 
   const handleKeyPress = (event) => {
-    if (event.key === 'Enter') {
+    if (event.key === "Enter") {
       sendMessage();
     }
   };
@@ -68,23 +69,44 @@ export default function Home() {
       flexDirection="column"
       justifyContent="center"
       alignItems="center"
-      bgcolor="black"
-      color="white"
+      color="#FFFFFF"
       p={2}
+      sx={{
+        backgroundImage: `url('/csbackground.png')`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+        backgroundRepeat: "no-repeat",
+        width: "100vw",
+        height: "100vh",
+      }}
     >
-      <Typography variant="h4" gutterBottom>
-        FBG Support Chat
+      <Typography
+        variant="h4"
+        gutterBottom
+        sx={{
+          color: "#fff",
+          backgroundColor: "#111",
+          padding: "20px",
+          borderRadius: "20px",
+          fontWeight: "bold",
+          textTransform: "uppercase",
+        }}
+      >
+        CS Chronicles Support Chat
       </Typography>
+
       <Stack
         direction="column"
-        width="600px"
+        width="1200px"
         height="700px"
-        border="1px solid white"
+        border="2px solid #E5383B"
         borderRadius={4}
         p={2}
         spacing={3}
         sx={{
-          backgroundColor: 'rgba(255, 255, 255, 0.1)',
+          backgroundColor:
+            "#111" /*background color for the chat box entirely*/,
+          color: "white",
         }}
       >
         <Stack
@@ -94,99 +116,109 @@ export default function Home() {
           overflow="auto"
           maxHeight="100%"
           sx={{
-            '&::-webkit-scrollbar': {
-              width: '6px',
+            "&::-webkit-scrollbar": {
+              width: "6px",
             },
-            '&::-webkit-scrollbar-track': {
-              background: 'grey',
+            "&::-webkit-scrollbar-track": {
+              background: "grey",
             },
-            '&::-webkit-scrollbar-thumb': {
-              background: 'darkgrey',
-              borderRadius: '4px',
+            "&::-webkit-scrollbar-thumb": {
+              background: "darkgrey",
+              borderRadius: "4px",
             },
           }}
         >
-          {
-            messages.map((message, index) => (
+          {messages.map((message, index) => (
+            <Box
+              key={index}
+              display="flex"
+              justifyContent={
+                message.role === "assistant" ? "flex-start" : "flex-end"
+              }
+            >
               <Box
-                key={index}
-                display="flex"
-                justifyContent={
-                  message.role === 'assistant' ? 'flex-start' : 'flex-end'
-                }
+                bgcolor={message.role === "assistant" ? "#800E13" : "#AD2831"}
+                color="white"
+                borderRadius={16}
+                padding={"23px"}
+                m={1}
+                boxShadow={3}
+                style={{
+                  color: "#FFF",
+                  border: "3px solid #111",
+                }}
               >
-                <Box
-                  bgcolor={
-                    message.role === 'assistant'
-                      ? 'primary.main'
-                      : 'secondary.main'
-                  }
-                  color="white"
-                  borderRadius={16}
-                  p={3}
-                  m={1}
-                  boxShadow={3}
-                  style={{
-                    color: 'white',
-                    border: '1px solid white',
-                  }}
-                >
-                  {message.content}
-                </Box>
+                {message.content}
               </Box>
-            ))
-          }
+            </Box>
+          ))}
         </Stack>
-        <Stack direction="row" spacing={2} alignItems="center">
+        <Stack direction="row" spacing={2} alignItems="center" color="white">
           <TextField
-            label="message"
+            label="Enter message here:"
             fullWidth
             multiline
-            maxRows={4}
+            maxRows={5}
             value={message}
             onChange={(e) => setMessage(e.target.value)}
             onKeyPress={handleKeyPress}
             InputProps={{
-              style: { color: 'white', border: '1px solid white', overflow: 'auto' },
+              style: {
+                color: "white",
+              },
             }}
             InputLabelProps={{
-              style: { color: 'white' }
+              style: {
+                color: "#FFF",
+                transition: "all 0.2s ease", // Smooth transition for the label
+              },
             }}
             sx={{
-              '& .MuiOutlinedInput-root': {
-                '& fieldset': {
-                  borderColor: 'white',
+              "& .MuiOutlinedInput-root": {
+                "& fieldset": {
+                  borderColor: "white",
+                  color: "white",
                 },
-                '&:hover fieldset': {
-                  borderColor: 'white',
+                "&:hover fieldset": {
+                  borderColor: "white",
+                  color: "white",
                 },
-                '&.Mui-focused fieldset': {
-                  borderColor: 'white',
+                "&.Mui-focused fieldset": {
+                  borderColor: "white",
+                  color: "white",
                 },
               },
-              '& .MuiInputBase-input': {
-                overflow: 'auto',
-                '&::-webkit-scrollbar': {
-                  width: '6px',
+              "& .MuiInputBase-input": {
+                overflow: "auto",
+                "&::-webkit-scrollbar": {
+                  width: "6px",
+                  color: "white",
                 },
-                '&::-webkit-scrollbar-track': {
-                  background: 'grey',
+                "&::-webkit-scrollbar-track": {
+                  background: "grey",
+                  color: "white",
                 },
-                '&::-webkit-scrollbar-thumb': {
-                  background: 'darkgrey',
-                  borderRadius: '4px',
+                "&::-webkit-scrollbar-thumb": {
+                  background: "darkgrey",
+                  borderRadius: "4px",
+                  color: "white",
                 },
               },
             }}
           />
+
           <Button
             variant="contained"
             onClick={sendMessage}
             sx={{
-              height: '56px',
-              bgcolor: 'primary.main',
-              '&:hover': {
-                bgcolor: 'primary.dark',
+              height: "56px",
+              bgcolor: "#660708",
+              fontWeight: "bold",
+              color: "#FFF",
+              "&:hover": {
+                bgcolor: "#A4161A",
+                fontWeight: "bold",
+                color: "#FFF",
               },
             }}
           >
