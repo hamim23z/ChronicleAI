a24bf08ebdf85404c319bb03dd3b4679593ae0c7
Hamim <hamimc232@gmail.com>
2024-08-10 17:51:58 -0400

    Basic Styling for Landing Page, will finish later tonight

--- app/landing/page.js
+++ app/landing/page.js
@@ -3,7 +3,19 @@ import Link from 'next/link';
 
 export default function LandingPage() {
   return (
-    <div>
+    <div
+      style={{
+        margin: 0,
+        padding: 0,
+        boxSizing: 'border-box',
+        width: '100%',
+        height: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
       <h1>Welcome to Our Website!</h1>
       <p>Click the button below to get started.</p>
       <Link href="/chat">
--- app/page.js
+++ app/page.js
@@ -1,4 +1,6 @@
 // app/page.js
+// This is in the app folder. And in the root. This just involves the redirect
+// aspect. When local host loads up, it takes the user to the landing page first.
 "use client";
 import RedirectComponent from "./redirect/redirectcomponent";
 
